89fe8f233c092252d63cac1bf6a14761b45cb70e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-10-15 15:02:34 +0200

    added integration with fortune API

--- main.ts
+++ main.ts
@@ -2,8 +2,16 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-app.use(ctx => {
-  ctx.response.body = "Hello deno!";
+const getRandomFortune = async (): Promise<string> => {
+  const response = await fetch("https://api.sen.cat/api/fortune");
+  const jsonBody: { fortune: string } = await response.json();
+  return jsonBody.fortune;
+  
+};
+
+app.use(async (ctx) => {
+  const fortune = await getRandomFortune();
+  ctx.response.body = `Your fortune:\n${fortune}`;
 });
 
 await app.listen({ port: 8080 });
